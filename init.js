5a77b95a793bf0de26548a18043807aab31d871f
Baalaandaa <baalaandaa@gmail.com>
2019-10-27 15:18:35 +0300

    Contest menu fix

--- init.js
+++ init.js
@@ -55,7 +55,7 @@ var init = async (CLIlogger, Browserlogger, headless) => {
             console.log(chalk.red.bold("Invalid login/password"));
         }
     }
-    await fs.writeFile(fs.readFileSync(require('os').homedir() + "/.pcms/.settings.json"), JSON.stringify({
+    await fs.writeFile(require('os').homedir() + "/.pcms/.settings.json", JSON.stringify({
         hostUrl: url,
         host: answers,
         login: data.name,
